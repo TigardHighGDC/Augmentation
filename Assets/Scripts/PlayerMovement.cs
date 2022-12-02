e054363991651ce72f72819309bca1ea4310cefe
Brandon <pacewicbrandon@gmail.com>
2022-12-02 07:18:44 -0800

    General cleanups

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,26 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+// Copyright (c) TigardHighGDC
+// SPDX-License SPDX-License-Identifier: Apache-2.0
+
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rb;
     private float x;
     private float y;
-    private Vector2 direction;
     public float speed;
+    private Rigidbody2D rb;
+    private Vector2 direction;
 
-    void Start()
+    private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
-    // Update is called with frequency of physics engine
-    void FixedUpdate()
+
+    private void FixedUpdate()
     {
         x = Input.GetAxisRaw("Horizontal");
         y = Input.GetAxisRaw("Vertical");
-        direction = new Vector2(x, y);
 
+        direction = new Vector2(x, y);
         direction.Normalize();
         rb.velocity = direction * speed;
     }
