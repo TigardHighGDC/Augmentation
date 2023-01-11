aee00b328ad9bb06328694c89169683beaab3a8b
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2023-01-10 16:36:28 -0800

    Enemy base class + A* Submodule (#38)
    * EnemyBase
    
    * Quick Change
    
    * Removed dir for PR merge
    
    * Git submodule A* (#41)
    
    * Update unity version (#34)
    
    * Typo fixes (#36)
    
    Apparently Grammarly has a visual studio code extension and it was able to catch a couple of errors. Take a [look](https://marketplace.visualstudio.com/items?itemName=znck.grammarly).
    
    * Added submodules
    
    * Added getting started bit, now that we need to update the submodule
    
    Co-authored-by: Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
    
    * Added copyright
    
    Co-authored-by: Brandon <pacewicbrandon@gmail.com>

---
+++ Assets/Scripts/Enemies/BasicEnemy.cs
@@ -0,0 +1,11 @@
+// Copyright (c) TigardHighGDC
+// SPDX-License SPDX-License-Identifier: Apache-2.0
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    public GameObject Player;
+}
