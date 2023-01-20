9e046f2aa2d23ab5075f6b599e2ad65cc6db50f8
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2023-01-19 22:31:14 -0800

    LaptopGoonAI

---
+++ Assets/Scripts/Enemies/LaptopGoonAI.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class LaptopGoonAI : MonoBehaviour
+{
+    AIPath aiPath;
+    AIDestinationSetter locationSetter;
+    public GameObject Player;
+    public float followDistance;
+    public float dangerDistance;
+    Seeker seeker;
+    bool runAway = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        aiPath = GetComponent<AIPath>();
+        locationSetter = GetComponent<AIDestinationSetter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookTowardsMovement();
+        keepDistance();
+    }
+
+    void keepDistance()
+    {
+        aiPath.isStopped = false;
+        float distance = DistanceBetweenPlayer(Player);
+        if (runAway && followDistance <= distance)
+        {
+            runAway = false;
+        }
+        else if (!runAway && dangerDistance >= distance)
+        {
+            runAway = true;
+        }
+
+        if (runAway)
+        {
+            seeker.StartPath(transform.position, (2 * transform.position) - Player.transform.position);
+        }
+        else
+        {
+            if (distance > followDistance)
+            {
+                seeker.StartPath(transform.position, Player.transform.position);
+            }
+            else
+            {
+                aiPath.isStopped = true;
+            }
+        }
+    }
+
+    float DistanceBetweenPlayer(GameObject player)
+    {
+        float y2 = player.transform.position.y;
+        float y1 = transform.position.y;
+        float x2 = player.transform.position.x;
+        float x1 = transform.position.x;
+        return Mathf.Sqrt(Mathf.Pow(y2 - y1, 2) + Mathf.Pow(x2 - x1, 2));
+    }
+
+    void LookTowardsMovement()
+    {
+        if (aiPath.desiredVelocity.x >= 0.15f)
+        {
+            transform.localScale = new Vector3(1,1,1);
+        }
+        else if (aiPath.desiredVelocity.y <= 0.15f)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        }
+    }
+}
