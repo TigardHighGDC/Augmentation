2cb97a01bd99cecfeb2ebf31662f3d72deffe472
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2023-02-07 14:04:42 -0800

    Update AudioManipulation.cs

--- Assets/Scripts/AudioManipulation.cs
+++ Assets/Scripts/AudioManipulation.cs
@@ -7,7 +7,7 @@ public class AudioManipulation : MonoBehaviour
     public static void BitCrusher(AudioClip original, int compression)
     {
         float[] samples = new float[original.samples * original.channels];
-        original.GetData(samples, 0);  
+        original.GetData(samples, 0);
         int chunk = original.samples / (int)Mathf.Pow(2, compression);
         for (int i = 0; i < samples.Length; i += chunk)
         {
