d516fe8a7d65f76a1b12564e211b8cc72120d085
Brandon <pacewicbrandon@gmail.com>
2023-02-10 09:16:05 -0800

    Initial review cms

--- Assets/Scripts/AudioManipulation.cs
+++ Assets/Scripts/AudioManipulation.cs
@@ -1,3 +1,6 @@
+// Copyright (c) TigardHighGDC
+// SPDX-License SPDX-License-Identifier: Apache-2.0
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,20 +11,25 @@ public class AudioManipulation : MonoBehaviour
     {
         float[] samples = new float[original.samples * original.channels];
         original.GetData(samples, 0);
-        int chunk = original.samples / (int)Mathf.Pow(2, compression);
+        int chunk = original.samples / (int) Mathf.Pow(2, compression);
+
         for (int i = 0; i < samples.Length; i += chunk)
         {
-            float chunkSum = 0f;
-            for (int u = i; u < i + chunk && u < samples.Length; u++)
+            float chunkSum = 0.0f;
+
+            for (int j = i; j < (i + chunk) && j < samples.Length; j++)
             {
-                chunkSum += samples[u];
+                chunkSum += samples[j];
             }
+
             chunkSum /= chunk;
-            for (int u = i; u < i + chunk && u < samples.Length; u++)
+
+            for (int j = i; j < (i + chunk) && j < samples.Length; j++)
             {
-                samples[u] = chunkSum;
+                samples[j] = chunkSum;
             }
         }
+
         original.SetData(samples, 0);
     }
 }
