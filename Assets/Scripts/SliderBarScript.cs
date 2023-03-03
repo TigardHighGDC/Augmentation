492a7ea5df50b30f39ae0f20a7fa793a82df1a8b
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2023-03-03 14:10:38 -0800

    Naming changes

--- Assets/Scripts/SliderBarScript.cs
+++ Assets/Scripts/SliderBarScript.cs
@@ -5,26 +5,24 @@ using UnityEngine.UI;
 
 public class SliderBarScript : MonoBehaviour
 {
-    public Slider slider;
-    // for color
-    public Gradient gradient;
-
-    public Image fill;
+    public Slider Slider;
+    public Gradient Gradient;
+    public Image Fill;
 
     // at start to set the number that it considers the max
     public void SetMaxHealth(float health)
     {
-        slider.maxValue = health;
-        slider.value = health;
+        Slider.maxValue = health;
+        Slider.value = health;
 
-        fill.color = gradient.Evaluate(1f);
+        Fill.color = Gradient.Evaluate(1f);
     }
 
     // for when you take damage it will shrink the fill
     public void SetHealth(float health)
     {
-        slider.value = health;
+        Slider.value = health;
 
-        fill.color = gradient.Evaluate(slider.normalizedValue);
+        Fill.color = Gradient.Evaluate(Slider.normalizedValue);
     }
 }
