a34cfbc4b8badb184502dfec15b298c7d9c3a093
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2023-03-03 14:12:46 -0800

    Comments

--- Assets/Scripts/SliderBarScript.cs
+++ Assets/Scripts/SliderBarScript.cs
@@ -9,7 +9,7 @@ public class SliderBarScript : MonoBehaviour
     public Gradient Gradient;
     public Image Fill;
 
-    // at start to set the number that it considers the max
+    // Set the number that it considers the max
     public void SetMaxHealth(float health)
     {
         Slider.maxValue = health;
@@ -18,7 +18,7 @@ public class SliderBarScript : MonoBehaviour
         Fill.color = Gradient.Evaluate(1f);
     }
 
-    // for when you take damage it will shrink the fill
+    // When you take damage it will shrink the fill
     public void SetHealth(float health)
     {
         Slider.value = health;
