a33b5432ea1ee26d9cb98019c050df9ba446b865
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2023-03-06 13:11:29 -0800

    Laser Created

---
+++ Assets/Scripts/Enemies/EY/LaserLine.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserLine : MonoBehaviour
+{
+    public bool DrawLine;
+    public Vector3 ChangePosition;
+    public Color Color;
+    public float Clear;
+    public float Width = 1f;
+
+    private LineRenderer line;
+    private Transform player;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        line = GetComponent<LineRenderer>();
+        Color[3] = Clear;
+        line.startColor = Color;
+        line.endColor = Color;
+        line.startWidth = Width;
+        line.endWidth = Width;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DrawLine)
+        {
+            line.SetPositions(new Vector3[] {transform.position + ChangePosition, player.position});
+        }
+        else
+        {
+            line.SetPositions(new Vector3[] {});
+        }
+        
+    }
+}
