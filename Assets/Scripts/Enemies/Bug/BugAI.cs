949639b5ca4890fc2125e7ffb13ff597a7091415
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2023-03-20 20:39:21 -0700

    Bug Update

--- Assets/Scripts/Enemies/Bug/BugAI.cs
+++ Assets/Scripts/Enemies/Bug/BugAI.cs
@@ -7,15 +7,27 @@ using UnityEngine;
 
 public class BugAI : EnemyAI
 {
+    private void MovementAnimation()
+    {
+        if (Mathf.Sign(aiPath.direction[0]) == Mathf.Sign(rb.velocity[0]))
+        {
+            anim.speed = (Mathf.Abs(aiPath.direction[0]) + Mathf.Abs(aiPath.direction[1])) /  2;
+        }
+        else
+        {
+            anim.speed = ((Mathf.Abs(aiPath.direction[0]) + Mathf.Abs(aiPath.direction[1])) /  2) * -1;
+        }
+    }
     private void Update()
     {
-        if (rb.velocity[0] >= 0.1f)
+        MovementAnimation();
+        if (aiPath.direction[0] >= 0.5)
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            transform.localScale = new Vector3(1, transform.localScale[1], transform.localScale[2]);
         }
-        else if (rb.velocity[0] <= -0.1f)
+        else if (aiPath.direction[0] <= -0.5)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-1, transform.localScale[1], transform.localScale[2]);
         }
     }
 
