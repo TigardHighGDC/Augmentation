468cc5f80a87141c2d0d6ebecc5d1c8b1db9e0ce
Brandon P <pacewicbrandon@gmail.com>
2023-04-12 08:57:41 -0700

    Merge main into `main-into-menu` (#150)

---
+++ Assets/Scripts/Map/MapLayer.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) TigardHighGDC
+// SPDX-License SPDX-License-Identifier: Apache-2.0
+
+using UnityEngine;
+
+[System.Serializable]
+public class MapLayer
+{
+    public NodeType NodeType;
+
+    public float MinDistanceFromPreviousLayer = 3.0f;
+    public float MaxDistanceFromPreviousLayer = 3.0f;
+
+    public float NodesApartDistance;
+
+    [Range(0.0f, 1.0f)]
+    public float RandomizePosition = 0.0f;
+
+    [Range(0.0f, 1.0f)]
+    public float RandomizeNodes = 0.0f;
+
+    [HideInInspector]
+    public FloatMinMax DistanceFromPreviousLayer =>
+        new FloatMinMax(MinDistanceFromPreviousLayer, MaxDistanceFromPreviousLayer);
+}
