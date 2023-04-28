b630ca846ebcf6833a18166c5477363629c61ba1
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2023-04-28 15:17:17 -0700

    Not sure if the sounds make the gameplay better

--- Assets/Scripts/Weapons/ProjectileGun/Bullet.cs
+++ Assets/Scripts/Weapons/ProjectileGun/Bullet.cs
@@ -11,15 +11,18 @@ public class Bullet : MonoBehaviour
     public static float C_Damage = 1f;
     public static int C_BulletPierce = 1;
     public static float C_Knockback = 1f;
+    [HideInInspector] public WeaponData Data;
 
-    [HideInInspector]
-    public WeaponData Data;
+    public AudioClip PlayerHitSFX;
+    public AudioClip EnemyHitSFX;
 
     private int remainingPierce;
+    private AudioSource audio;
 
     private void Start()
     {
         Invoke("DestroyBullet", Data.DespawnTime);
+        audio = GameObject.FindWithTag("UI_SFX").GetComponent<AudioSource>();
         remainingPierce = Data.BulletPierce;
     }
 
@@ -33,18 +36,30 @@ public class Bullet : MonoBehaviour
                 transform.up * (Data.Knockback * CorruptionLevel.KnockbackIncrease * C_Knockback), ForceMode2D.Impulse);
             nonPlayerHealth.Damage(Data.Damage * C_Damage);
 
-            // Calls current bullet hit modifiers
-            ItemHandling.BulletHit?.Invoke();
-            remainingPierce--;
+            audio.clip = EnemyHitSFX;
+            audio.Play();
 
+            remainingPierce--;
             if (remainingPierce * C_BulletPierce <= 0)
             {
                 DestroyBullet();
             }
-
             break;
         case "Wall":
             DestroyBullet();
+            break;
+        case "Player":
+            PlayerHealth PlayerHealth = collide.gameObject.GetComponent<PlayerHealth>();
+            PlayerHealth.Damage(Data.Damage * C_Damage);
+
+            audio.clip = PlayerHitSFX;
+            audio.Play();
+
+            remainingPierce--;
+            if (remainingPierce * C_BulletPierce <= 0)
+            {
+                DestroyBullet();
+            }
             break;
         }
     }
