65168c959e95c4489bfa121d82de87a8dfdfdc5f
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2023-05-04 15:34:44 -0700

    Store Partially Finished

--- Assets/Scripts/Item/ItemStorage.cs
+++ Assets/Scripts/Item/ItemStorage.cs
@@ -10,6 +10,7 @@ public class ItemStorage : MonoBehaviour
     public static GameObject Fragmentation = null;
     public static GameObject Weapon = null;
     public static List<GameObject> ItemList = new List<GameObject>();
+    public static List<int> ResourceItemIndex = new List<int>();
 
     // Position for item placement on screen
     private static int uiX = 0;
